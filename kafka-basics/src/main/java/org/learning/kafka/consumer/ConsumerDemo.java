1960f157af463236892630a45b0e23e55449a645
MaksimKavalenka <25153828+MaksimKavalenka@users.noreply.github.com>
2023-07-23 17:52:58 +0300

    Consumer demo

--- kafka-basics/src/main/java/org/learning/kafka/consumer/ConsumerDemo.java
+++ kafka-basics/src/main/java/org/learning/kafka/consumer/ConsumerDemo.java
@@ -1,34 +1,39 @@
-package org.learning.kafka.producer;
+package org.learning.kafka.consumer;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.learning.kafka.KafkaProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
-public class ProducerDemo {
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
 
     private static final String TOPIC = "java_topic";
 
     public static void main(String[] args) {
-        Properties properties = getProperties();
+        Properties properties = KafkaProperties.CONSUMER.getProperties();
 
-        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC, "hello world");
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
+            consumer.subscribe(Collections.singleton(TOPIC));
 
-            producer.send(producerRecord);
-            producer.flush();
+            while (true) {
+                log.info("Polling");
+
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info(String.format("Key: %s, Value: %s", record.key(), record.value()));
+                    log.info(String.format("Partition: %d, Offset: %d", record.partition(), record.offset()));
+                }
+            }
         }
-    }
-
-    private static Properties getProperties() {
-        Properties properties = new Properties();
-
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
-
-        return  properties;
     }
 
 }
